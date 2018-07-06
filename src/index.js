68a7e65fea06fe33dde029ee3fdb83e7c6c11195
Jan Fischer <jan.fischer@nexum.de>
2018-07-06 17:07:41 +0200

    render components to html files

--- src/index.js
+++ src/index.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import styled from '../lib/styler';
-import reactHexagon from './assets/react-hexagon.png';
+
+// import components
 import Heading from './components/atomic/atoms/Heading/Heading';
 import Paragraph from './components/atomic/atoms/Paragraph/Paragraph';
 import ButtonSwitch from './components/atomic/atoms/ButtonSwitch/ButtonSwitch';
 import Block from './components/atomic/molecules/Block/Block';
 
-const Index = () => {
-  return (
-    <React.Fragment>
-      <Heading>Aiiight</Heading>
-      <Paragraph>Was geht?</Paragraph>
-      <Block heading="Über children?" paragraph="Geht doch.." />
-      <ButtonSwitch />
-      <div><img src={reactHexagon} width="50" height="50" /></div>
-    </React.Fragment>
-  );
+const componentRegistry = {
+  'Heading': Heading,
+  'Paragraph': Paragraph,
+  'ButtonSwitch': ButtonSwitch,
+  'Block': Block
 };
 
-ReactDOM.render(<Index />, document.getElementById('index'));
+const reactComponents = document.querySelectorAll('[data-reactcomponent]');
+
+for(var i in reactComponents) {
+  if (!reactComponents.hasOwnProperty(i)) {
+    continue;
+  }
+
+  const container = reactComponents[i];
+  const component = componentRegistry[container.dataset['reactcomponent']];
+  const props = container.dataset['reactdata'] ? JSON.parse(container.dataset['reactdata']) : {};
+
+  if (component) {
+    ReactDOM.hydrate(
+      React.createElement(
+        component,
+        props
+      ),
+      container
+    );
+  }
+}
