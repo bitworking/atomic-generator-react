ac165313251ee283ea0ebc0f5c3fc487322c7614
Jan Fischer <info@bitworking.de>
2019-02-22 00:53:03 +0100

    Rename reactdata -> reactprops, outsource componentRegistry file

---
+++ src/componentRegistry.js
@@ -0,0 +1,13 @@
+import Heading from './components/atomic/atoms/Heading/Heading';
+import Paragraph from './components/atomic/atoms/Paragraph/Paragraph';
+import ButtonSwitch from './components/atomic/atoms/ButtonSwitch/ButtonSwitch';
+import Block from './components/atomic/molecules/Block/Block';
+
+// Only dynamic components need to be defined
+
+export default {
+  // 'atoms-heading-h1': Heading,
+  // 'atoms-paragraph': Paragraph,  
+  // 'molecules-block': Block,
+  'atoms-buttonswitch': ButtonSwitch,
+};
