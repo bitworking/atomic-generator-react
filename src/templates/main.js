bfa0e7f5fc3e40480029a0ed40cb3405c9d63fc8
Jan Fischer <info@bitworking.de>
2019-03-01 02:07:51 +0100

    atomic-renderer refactor/rewrite

--- src/templates/main.js
+++ src/templates/main.js
@@ -6,11 +6,11 @@ module.exports = body => `
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Atomic Generator React</title>
-  <link href="../../../assets/css/main.css" type="text/css" rel="stylesheet">
+  <link href="../assets/css/main.css" type="text/css" rel="stylesheet">
 </head>
 <body>
   ${body}
-  <script src="../../../assets/js/main.js"></script>
+  <script src="../assets/js/main.js"></script>
 </body>
 </html>
 `;
